ee053a0311a426299150dcf4564ff5e0a1d855a0
Bhanvi Gautam <72307207+bhanvi-gautam@users.noreply.github.com>
2023-12-25 13:19:22 +0530

    Create navbar.php

---
+++ partials/navbar.php
@@ -0,0 +1,94 @@
+<nav class="navbar navbar-custom navbar-inverse navbar-fixed-top bg-dark text-white" role="navigation" style="color: black">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                    </button>
+                <a class="navbar-brand page-scroll" href="index.php">
+                Wheels4you </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+
+            <?php
+                if(isset($_SESSION['login_client'])){
+            ?> 
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_client']; ?></a>
+                    </li>
+                    <li>
+                    <ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Control Panel <span class="caret"></span> </a>
+                <ul class="dropdown-menu" style="background-color: black ;color:white">
+              <li> <a href="entercar.php">Add Car</a></li>
+              <li> <a href="enterdriver.php"> Add Driver</a></li>
+              <li> <a href="viewavailablecars.php"> View Available Cars</a></li>
+              <li> <a href="clientview.php">View Cars Booked</a></li>
+
+            </ul>
+            </li>
+          </ul>
+                    </li>
+                    <li>
+                        <a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                    </li>
+                </ul>
+            </div>
+            
+            <?php
+                }
+                else if (isset($_SESSION['login_customer'])){
+            ?>
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_customer']; ?></a>
+                    </li>
+                    <ul class="nav navbar-nav">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> GARAGE <span class="caret"></span> </a>
+                <ul class="dropdown-menu" style="background-color: black ;color:white">
+              <li> <a href="prereturncar.php">Return Now</a></li>
+              <li> <a href="mybookings.php"> My Bookings</a></li>
+            </ul>
+            </li>
+          </ul>
+                    <li>
+                        <a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                    </li>
+                </ul>
+            </div>
+
+            <?php
+            }
+                else {
+            ?>
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="clientlogin.php">Employee</a>
+                    </li>
+                    <li>
+                        <a href="customerlogin.php">Customer</a>
+                    </li>
+                    <li>
+                        <a href="faq/index.php"> FAQ </a>
+                    </li>
+                </ul>
+            </div>
+                <?php   }
+                ?>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
